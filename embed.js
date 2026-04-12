3140d59235f28e1d304e5791636ceadf8c951b76
aopontann <aopontan0416@gmail.com>
2026-04-12 12:47:13 +0900

    細かい修正

--- embed.js
+++ embed.js
@@ -53,5 +53,5 @@ const send = async (url, content) => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ content: content }),
-    });
+    })
 }
